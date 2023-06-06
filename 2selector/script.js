89b6a1db68c751d86143bd2f70aedf71dc5bd72e
noodles4u <louisdesmett@gmail.com>
2023-06-06 14:19:55 +0200

    remember scope

--- 2selector/script.js
+++ 2selector/script.js
@@ -9,10 +9,29 @@ const important = document.getElementsByClassName('important');
 for (const attri of important) {
   attri.setAttribute('title', 'teeest');
 }
+
 // hide images without .important class
 for (const imgDisplay of document.images) {
   if (imgDisplay.className !== 'important') {
     imgDisplay.setAttribute('style', 'display:none');
     console.log('Images are hidden');
   }
+}
+
+const allPara = document.getElementsByTagName('p');
+
+let child;
+for (child of allPara) {
+  if (child.getAttribute('class', '')) {
+    console.log(`(Class: ${child.className}) ${child.innerText}`);
+  } else {
+    console.log(child.innerText);
+  }
+}
+
+for (child of allPara) {
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+  child.style.color = `#${randomColor}`;
+  console.log(`#${randomColor}`);
 }
