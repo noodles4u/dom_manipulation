3bf74cb6b313e668a513c4620871453253ac37ae
noodles4u <louisdesmett@gmail.com>
2023-06-07 12:16:27 +0200

    add hsl color randomizer and convert l data to number in other function

--- 3create/script.js
+++ 3create/script.js
@@ -10,6 +10,16 @@ const nameArray = ['Pierre', 'Paul', 'Panda'];
 // color test
 // artPos.style.background = 'rgb(20, 24, 245)';
 
+// HSL color randomizer
+const randomColor = () => {
+  const h = Math.floor(Math.random() * 360);
+  s = Math.floor(Math.random() * 100) + '%';
+  l = Math.floor(Math.random() * 100) + '%'; //full
+
+  return `hsl(${h},${s},${l})`;
+};
+//
+
 // section creation (REMEMBER scope)
 const amount = 3;
 const sectionCr = (secNr) => {
@@ -22,13 +32,26 @@ sectionCr(amount);
 
 // paragraph creation and append to section elements (usage of array)
 const pCr = () => {
-  const s = document.querySelectorAll('section');
+  const sect = document.querySelectorAll('section');
 
-  for (let i = 0; i < s.length; i++) {
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  for (let i = 0; i < sect.length; i++) {
+    // var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
     const newP = document.createElement('p');
-    newP.style.background = `#${randomColor}`;
-    s[i].append(newP);
+    newP.style.background = `${randomColor()}`;
+    sect[i].append(newP);
+
+    // convert l of hsl to number and remove  %
+    var lNumber = l.replace('%', '');
+    console.log(lNumber);
+
+    if (lNumber > 50) {
+      console.log('too bright');
+      newP.style.color = 'black';
+    } else {
+      console.log('too dark');
+      newP.style.color = 'white';
+    }
   }
 };
 pCr();
