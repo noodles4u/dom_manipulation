a4b90ba67132234661f2eb0147e159657e0752d9
noodles4u <louisdesmett@gmail.com>
2023-06-07 11:32:27 +0200

    paragraph name content loop

--- 3create/script.js
+++ 3create/script.js
@@ -4,33 +4,44 @@
 console.log('OK TEST GO');
 //
 
-const newP = document.createElement('p');
-// const newStuff = document.createTextNode(
-//   'Running up that hill, shouting at the sky',
-// );
-
-const nameArray = ['Pierre', 'Paul', 'Panda'];
-// const nameTag = 'pierre';
-
 const artPos = document.getElementsByTagName('article')[0];
+const nameArray = ['Pierre', 'Paul', 'Panda'];
 
 // color test
-artPos.style.background = 'rgb(20, 24, 245)';
+// artPos.style.background = 'rgb(20, 24, 245)';
 
-// const randoColorP = (amountElements) => {
-//   for (let i = 0; i < amountElements; i++) {
-//     newSection.appendChild(newStuff);
-//     newSection.append(newP);
-//     newP.append(nameTag);
-//   }
-// };
-
-// randoColorP(3);
-let s = document.querySelectorAll('section');
+// section creation (REMEMBER scope)
+const amount = 3;
 const sectionCr = (secNr) => {
   for (let i = 0; i < secNr; i++) {
     const newSection = document.createElement('section');
     artPos.append(newSection);
   }
 };
-sectionCr(3);
+sectionCr(amount);
+
+// paragraph creation and append to section elements (usage of array)
+const pCr = () => {
+  const s = document.querySelectorAll('section');
+
+  for (let i = 0; i < s.length; i++) {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const newP = document.createElement('p');
+    newP.style.background = `#${randomColor}`;
+    s[i].append(newP);
+  }
+};
+pCr();
+
+// add names to paragraph
+
+const addNames = () => {
+  const para = document.querySelectorAll('section > p');
+
+  for (let i = 0; i < para.length; i++) {
+    const names = document.createTextNode(nameArray[i]);
+    para[i].append(names);
+    console.log(`test ${[i]}`);
+  }
+};
+addNames();
