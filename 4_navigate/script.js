16d54fd07387fcf29cd1db6711a4b19f34d2e477
noodles4u <louisdesmett@gmail.com>
2023-06-07 15:16:03 +0200

    lastChildElement dom navigation

--- 4_navigate/script.js
+++ 4_navigate/script.js
@@ -1,2 +1,7 @@
 /*
-*/
+ */
+
+const orderedL = document.querySelector('ol');
+orderedL.children[1].style.background = 'red';
+orderedL.insertBefore(orderedL.lastElementChild, orderedL.children[0]);
+orderedL.lastElementChild.style.color = 'blue'; // new last child got colored
