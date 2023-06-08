a4ddedbaebfb372bbadfc1f315da1a4ebb4ad52e
noodles4u <louisdesmett@gmail.com>
2023-06-08 14:01:56 +0200

    appended creation information in list

--- 5_events/script.js
+++ 5_events/script.js
@@ -1,16 +1,39 @@
-const _initTime = Date.now()
+const _initTime = Date.now();
 
 const getElapsedTime = () => {
-  return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
-}
+  return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's';
+};
+
+// my code
+const displayed = document.querySelector('.displayedsquare-wrapper');
+const list = document.querySelector('ul');
+
+// const newSquares = () => {
+//   const div = document.createElement('div');
+//   div.setAttribute('class', 'actionsquare displayedsquare violet');
+//   displayed.append(div);
+// };
+// //
 
 const clickOnSquare = (e) => {
-  console.log(e.target.classList[1])
-  console.log(getElapsedTime())
-}
+  // console.log(e.target.classList[1]);
+  // console.log(getElapsedTime());
+  const classAndTime = `
+  ${getElapsedTime()}
+  Created a new ${e.target.classList[1]} square`;
+  // newSquares();
+  const div = document.createElement('div');
+  div.setAttribute(`class`, `displayedsquare ${e.target.classList}`);
+  displayed.append(div);
 
-const actionSquares = document.querySelectorAll('.actionsquare')
+  const li = document.createElement('li');
+  li.append(classAndTime);
+  list.append(li);
+};
+
+//
+
+const actionSquares = document.querySelectorAll('.actionsquare');
 for (let actionSquare of actionSquares) {
-  actionSquare.addEventListener('click', clickOnSquare)
+  actionSquare.addEventListener('click', clickOnSquare);
 }
-
