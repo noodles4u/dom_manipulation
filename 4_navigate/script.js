98e6f2db388007f53e53b96be50e7091e80200f9
noodles4u <louisdesmett@gmail.com>
2023-06-08 11:28:58 +0200

    research nodeList more indepth

--- 4_navigate/script.js
+++ 4_navigate/script.js
@@ -11,10 +11,9 @@ orderedL.lastElementChild.style.color = 'blue'; // new last child got colored
 // second part exo
 const sections = document.querySelectorAll('section');
 const thirdSec = sections[2];
-// thirdSec.style.background = 'red';
 
 const h2 = document.querySelectorAll('h2');
-// thirdSec.firstElementChild.style.color = 'yellow';
+thirdSec.firstElementChild.style.color = 'yellow';
 
 sections[1].insertBefore(h2[2], sections[1].children[0]);
 sections[2].insertBefore(h2[1], sections[2].children[0]);
@@ -24,3 +23,17 @@ const body = document.querySelector('body');
 // body.style.background = 'red';
 const kids = body.children;
 console.log(body.children);
+console.log(kids);
+
+// not necessary but fun, create array from HTML collection
+Array.from(kids).forEach((kid) => {
+  kid.style.backgroundColor = 'red';
+});
+
+// document.body.lastChildElement.remove();
+console.log(document.body.lastChild.text);
+let bodyNode = document.querySelectorAll('body');
+
+console.log(body.children[body.children.length - 2]);
+let footer = body.children[body.children.length - 2];
+footer.remove();
