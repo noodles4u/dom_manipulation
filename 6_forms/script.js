64d3b85fe54624ff7a51937b0702298bd98f5d3e
noodles4u <louisdesmett@gmail.com>
2023-06-09 12:09:31 +0200

    input lenght checker with error message"

--- 6_forms/script.js
+++ 6_forms/script.js
@@ -6,21 +6,36 @@ document.body.style.padding = '20px';
 
 console.log('test');
 
-const section = document.getElementsByTagName('section');
+const sections = document.getElementsByTagName('section');
 const input1 = document.getElementById('firstname');
-console.log(section);
-console.log(input1);
-// section[0].input.append('uhu');
-input1.append('uhu');
+const displayText = document.getElementById('display-firstname');
 
-section[0].addEventListener('keyup', (e) => {
+const checkLength = (check) => {
+  if (check.length < 6) {
+    console.log('type more');
+    console.log(check.length);
+  }
+};
+sections[0].addEventListener('keyup', (e) => {
   if (e.target.id == 'firstname') {
-    document.body.style.background = 'red';
-    if (e.key.length === 1) {
-      document.getElementById('display-firstname').append(e.key);
-    }
+    document.body.style.background = 'coral';
+    //   if (e.key.length === 1) {
+    //     document.getElementById('display-firstname').append(e.key);
+    //   }
+  }
+  const check = (displayText.innerHTML = e.target.value);
+  checkLength(check);
+});
+
+/// try to remove negative numbers
+sections[1].addEventListener('keyup', (e) => {
+  if (e.target.value < 18) {
+    console.log('you young');
+  } else {
+    document.getElementById('a-hard-truth').style.visibility = 'visible';
   }
 });
+
 init = () => {
   // Clear forms here
   document.getElementById('firstname').value = '';
