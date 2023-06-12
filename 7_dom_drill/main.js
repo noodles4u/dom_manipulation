e1a8160c347866cdb9d725ecf7cf073a024669d3
noodles4u <louisdesmett@gmail.com>
2023-06-12 11:46:41 +0200

    new code with double for loop as to compare different indexes

--- 7_dom_drill/main.js
+++ 7_dom_drill/main.js
@@ -1,5 +1,3 @@
-console.log('yeehaa');
-
 const list = document.querySelectorAll('li');
 const ul = document.querySelector('ul');
 // console.log(list);
@@ -12,22 +10,86 @@ console.log(ul.childNodes);
 // const findStuff = test.find((f) => {
 //   return f.innerText === 'Fast and Furious';
 // });
-ul.style.cursor = 'pointer';
-console.log(ul.childNodes[0]);
-for (each of ul.childNodes) {
-  if (each.nodeType === 1) {
-    console.log(each.innerText);
-    if (each.innerText === 'Fast and Furious') {
-      console.log('found');
-      each.setAttribute('class', 'important');
 
-      ul.insertBefore(each, ul.childNodes[0]);
-    }
-    each.addEventListener('click', (e) => {
-      alert(e.target.innerHTML);
+const importantMessage =
+  "The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.";
+
+/// New with 2 for loops
+const itemsLi = ul.childNodes;
+//
+// loop 1
+for (let i = 0; i < itemsLi.length; i++) {
+  if (itemsLi[i].nodeType === 1) {
+    // console.log(itemsLi[i].innerText);
+    //style
+    itemsLi[i].style.cursor = 'pointer';
+
+    itemsLi[i].addEventListener('click', (e) => {
+      if (e.target.className === 'important') {
+        alert(importantMessage);
+      } else alert(e.target.innerHTML);
     });
+
+    if (itemsLi[i].innerText === 'Fast and Furious') {
+      console.log('found');
+      itemsLi[i].setAttribute('class', 'important');
+
+      ul.insertBefore(itemsLi[i], ul.childNodes[0]);
+    }
+
+    //
+    // loop 2
+    for (let a = 0; a < ul.childNodes.length; a++) {
+      if (itemsLi[i].isEqualNode(itemsLi[a]) && itemsLi[i] !== itemsLi[a]) {
+        console.log('removed');
+        ul.removeChild(itemsLi[a]);
+      }
+    }
   }
 }
+
+// ///// OLD
+// for (each of ul.childNodes) {
+//   if (each.nodeType === 1) {
+//     //style
+//     each.style.cursor = 'pointer';
+
+//     // list items
+//     console.log(each.innerText);
+
+//     // for (let i = 0; i < ul.childNodes.length; i++) {
+//     //   if (each.isEqualNode(ul.childNodes[i]) && ul.childNodes !== i) {
+//     //     console.log(true);
+//     //   }
+//     // }
+//     //   if (each.innerText == ul.childNodes[i].innerText) {
+//     //     console.log(each.innerText);
+//     //     console.log(ul.childNodes[i].innerText);
+//     //   }
+//     // }
+
+//     // if (ul.childNodes[0].innerText.isEqualNode(each.innerText)) {
+//     //   console.log(each);
+//     //   console.log('yeeeeeeeeeeee');
+//     // }
+
+//     if (each.innerText === 'Fast and Furious') {
+//       console.log('found');
+//       each.setAttribute('class', 'important');
+
+//       ul.insertBefore(each, ul.childNodes[0]);
+//     }
+
+//     each.addEventListener('click', (e) => {
+//       if (e.target.className === 'important') {
+//         alert(importantMessage);
+//       } else alert(e.target.innerHTML);
+//     });
+//   }
+// }
+
+// TESTS
+//
 // console.log(ul.childNodes);
 // console.log(ul.children[0]);
 // console.log(ul.children[ul.children.length - 1].innerHTML);
