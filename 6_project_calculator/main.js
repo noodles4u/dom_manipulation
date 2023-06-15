3a6b29195696e7e089c56f0779ae5f46a5bd49f2
noodles4u <louisdesmett@gmail.com>
2023-06-15 11:09:28 +0200

    setup + result button code without eval

---
+++ 6_project_calculator/main.js
@@ -0,0 +1,9 @@
+console.log('hello');
+// The "result" button
+// function computeResult(str) {
+//   return Function('return ' + str)();
+// }
+
+// const a = '(13 + 17) / 3';
+
+// console.log(computeResult(a)); // Should display 10
