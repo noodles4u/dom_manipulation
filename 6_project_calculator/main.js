a69af8ba09f94f2988616ef90286d1fc2d740d02
noodles4u <louisdesmett@gmail.com>
2023-06-15 11:37:45 +0200

    forEach with calc elemnts in grid layout

--- 6_project_calculator/main.js
+++ 6_project_calculator/main.js
@@ -1,9 +1,54 @@
-console.log('hello');
+console.log('%chello', 'color:blue');
 // The "result" button
-// function computeResult(str) {
-//   return Function('return ' + str)();
-// }
+function computeResult(str) {
+  return Function('return ' + str)();
+}
 
-// const a = '(13 + 17) / 3';
+const a = '(13 + 17) / 3';
 
-// console.log(computeResult(a)); // Should display 10
+console.log(computeResult(a)); // Should display 10
+let main = document.querySelector('main');
+
+let calcSection = document.createElement('section');
+main.append(calcSection);
+
+let calcArray = [
+  '(',
+  ')',
+  '%',
+  'AC',
+  7,
+  8,
+  9,
+  '/',
+  4,
+  5,
+  6,
+  '*',
+  1,
+  2,
+  3,
+  '-',
+  0,
+  '.',
+  '=',
+  '+',
+];
+console.log(calcArray.length);
+
+////
+const createBlocks = (blocks) => {
+  calcArray.forEach((item) => {
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    p.innerText = item;
+    div.style = 'border: 1px dotted gray; background:white';
+    p.style = 'text-align:center';
+    div.append(p);
+    calcSection.append(div);
+  });
+};
+
+createBlocks(20);
+calcSection.style =
+  'background:coral; padding: 1em; font-family:system-ui;display:grid;gap: 1rem; grid-template-columns:repeat(4,1fr);';
